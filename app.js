f7f0947e6f54e7ce36b5e7964b7c2a9a8ebf1241
lifen <lifen@yy.com>
2018-04-13 18:32:55 +0800

    to be continued

--- app.js
+++ app.js
@@ -2,19 +2,37 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const shell = require('shelljs');
 
+const config = require('./utils/const');
+const LOCAL_PATH = config.LOCAL_PATH;
+const _PORT_ = config._PORT_;
+
 const app = express();
 
 app.use(bodyParser.json({limit: '1024kb'}));
 
 app.get('/', (req, res) => {
-  res.send('fg2s is running!');
+  res.send(`fg2s is running on ${_PORT_}!`);
 });
 
 app.post('/', (req, res) => {
-  const body = req.body || {};
+  const body = req.body || {}; 
 
+  // 非push, 不同步
+  if (body.object_kind !== 'push') {
+    res.send('It is not a push event.');
+    return false;
+  } else {
+  	const projectName = body.project.name; // 项目名称
+  	const branchName = body.ref.replace('refs/heads/', ''); // 分支名称
+  	const gitUrl = body.project.git_ssh_url; // repos 的 git 地址
+  	const projectPath = body.project.path_with_namespace; // 带名字空间的项目地址
+  	const projectGroup = projectPath.replace(`/${projectName}`, '').toLowerCase(); // 组名
+  	
+
+
+  }
 });
 
-app.listen(1993, () => {
-  console.log('fg2s listening on port 1993!');
+app.listen(_PORT_, () => {
+  console.log(`fg2s listening on port ${_PORT_}!`);
 });
---
+++ utils/config.js
@@ -0,0 +1,13 @@
+exports._PORT_ = 1993; // 服务监听端口
+
+exports.LOCAL_PATH = '/var/git2svn'; 
+
+exports.GROUP_LIST = {
+  GIT_GROUP: [
+    '' // gitLab上对应的羡慕组
+  ]
+};
+
+exports.REMOTE_PATH_LIST = {
+  SVN_PATH: '' // SVN地址
+};
